7d292ed6e629a6063ea5027fa9dc248171fdfca3
Rafay Qayyum <49898477+Rafayqayyum@users.noreply.github.com>
2022-01-04 22:54:23 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,122 @@
+from turtle import getscreen, Turtle
+import winsound
+#creates a screen and sets its attributes
+wn = getscreen()
+wn.title("Pubg")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+#score variables
+scoreA = 0
+scoreB = 0
+count = 0
+# Paddle A
+paddle_a = Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+# Paddle B
+paddle_b = Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+# Ball
+ball = Turtle()
+ball.speed()
+ball.shape("circle")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 0.3
+ball.dy = 0.3
+
+# Pen
+pen = Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("PlayerA: 0   PlayerB: 0", align="center", font=("Courier", 24, "normal"))
+
+#paddle movement function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 30
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 30
+    paddle_a.sety(y)
+
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 30
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 30
+    paddle_b.sety(y)
+
+
+# Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
+while True:
+    wn.update()
+    # Move Ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        winsound.PlaySound("bounce.mp3", winsound.SND_ASYNC)
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        winsound.PlaySound("bounce.mp3", winsound.SND_ASYNC)
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        scoreA += 1
+        pen.clear()
+        winsound.PlaySound("bounce.mp3", winsound.SND_ASYNC)
+        pen.write("PlayerA: {}   PlayerB: {}".format(scoreA, scoreB), align="center", font=("Courier", 24, "normal"))
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        scoreB += 1
+        winsound.PlaySound("bounce.mp3", winsound.SND_ASYNC)
+        pen.clear()
+        pen.write("PlayerA: {}   PlayerB: {}".format(scoreA, scoreB), align="center", font=("Courier", 24, "normal"))
+    # Paddle Ball hits
+    if (340 < ball.xcor() < 350) and (paddle_b.ycor() + 50 > ball.ycor() > paddle_b.ycor() - 50):
+        ball.setx(340)
+        ball.dx *= -1
+        winsound.PlaySound("bounce.mp3", winsound.SND_ASYNC)
+    if (-340 > ball.xcor() > -350) and (paddle_a.ycor() + 50 > ball.ycor() > paddle_a.ycor() - 50):
+        ball.setx(-340)
+        ball.dx *= -1
+        winsound.PlaySound("bounce.mp3", winsound.SND_ASYNC)
+    if count == 50:
+        count = 0
+        ball.dx += 0.1
+        ball.dy += 0.1
